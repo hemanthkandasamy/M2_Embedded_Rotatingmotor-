701d0b046e11e36fde8e20f7da163962df9b79bb
hemanthkandasamy <94169511+hemanthkandasamy@users.noreply.github.com>
2021-12-01 17:44:48 +0530

    Update Project_main.c

--- 3_Implementation/Project_main.c
+++ 3_Implementation/Project_main.c
@@ -1,1 +1,21 @@
+#include <avr/io.h>
+#define F_CPU 1000000
+#include <util/delay.h>
 
+
+int main(void)
+{
+	DDRA = 0x81;
+	
+	while(1)
+	{
+		PORTA = 0x01;
+		_delay_ms(1000);
+		PORTA = 0x00;
+		_delay_ms(500);
+		PORTA = 0x80;
+		_delay_ms(1000);
+		PORTA = 0x00;
+		_delay_ms(500);
+	}
+}
